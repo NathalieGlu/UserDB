ef1963ef6911e4ed4561cdd896fb512dfe464b17
n.gluschenko <n.gluschenko@qiwi.com>
2018-03-14 16:41:41 +0300

    fix

--- src/main/java/ru/nathalie/handler/ErrorHandler.java
+++ src/main/java/ru/nathalie/handler/ErrorHandler.java
@@ -3,10 +3,9 @@ package ru.nathalie.handler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.nathalie.model.data.ResponseData;
-import ru.nathalie.web.WebServer;
 
 public class ErrorHandler {
-    private static final Logger log = LoggerFactory.getLogger(WebServer.class.getName());
+    private static final Logger log = LoggerFactory.getLogger(ErrorHandler.class.getName());
 
     public String getException(Throwable throwable) {
         ResponseData responseData = new ResponseData();
